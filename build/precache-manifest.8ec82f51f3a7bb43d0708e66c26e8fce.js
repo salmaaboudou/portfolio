ac16381aa710c8d5698831a987c0956825b9647d
Salma Aboudou <salmaaboudou@MacBook-Pro-de-Salma.local>
2023-01-05 18:02:52 +0100

    first commit

---
+++ build/precache-manifest.8ec82f51f3a7bb43d0708e66c26e8fce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d15005705952e6171dbbe00dc919ebe",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4a596786cf1909dac402",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f318675c4c0b5d293dad",
+    "url": "/portfolio/static/css/main.356f7c76.chunk.css"
+  },
+  {
+    "revision": "4a596786cf1909dac402",
+    "url": "/portfolio/static/js/2.28431c08.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.28431c08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f318675c4c0b5d293dad",
+    "url": "/portfolio/static/js/main.f0e3055a.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "81bff4dd7685f53fd6569430d423f349",
+    "url": "/portfolio/static/media/computer.81bff4dd.svg"
+  }
+]);
---
+++ node_modules/react-vertical-timeline-component/dist-es6/VerticalTimeline.js
@@ -0,0 +1,42 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+var _classnames = _interopRequireDefault(require("classnames"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const VerticalTimeline = ({
+  animate,
+  className,
+  layout,
+  children
+}) => /*#__PURE__*/_react.default.createElement("div", {
+  className: (0, _classnames.default)(className, 'vertical-timeline', {
+    'vertical-timeline--animate': animate,
+    'vertical-timeline--two-columns': layout === '2-columns',
+    'vertical-timeline--one-column-left': layout === '1-column' || layout === '1-column-left',
+    'vertical-timeline--one-column-right': layout === '1-column-right'
+  })
+}, children);
+
+VerticalTimeline.propTypes = {
+  children: _propTypes.default.oneOfType([_propTypes.default.arrayOf(_propTypes.default.node), _propTypes.default.node]).isRequired,
+  className: _propTypes.default.string,
+  animate: _propTypes.default.bool,
+  layout: _propTypes.default.oneOf(['1-column-left', '1-column', '2-columns', '1-column-right'])
+};
+VerticalTimeline.defaultProps = {
+  animate: true,
+  className: '',
+  layout: '2-columns'
+};
+var _default = VerticalTimeline;
+exports.default = _default;
