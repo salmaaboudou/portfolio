f5916db14ba297f8d312d32d907c440c9ad497e6
Salma <aboudou.salma@live.fr>
2023-04-17 22:00:16 +0200

    Changement font

--- src/components/About.js
+++ src/components/About.js
@@ -73,7 +73,7 @@ class About extends Component {
                     }}
                   >
                     <br />
-                    <span className="wave">{hello} :) </span>
+                    <span className="wave">{hello} </span>
                     <br />
                     <br />
                     {about}
